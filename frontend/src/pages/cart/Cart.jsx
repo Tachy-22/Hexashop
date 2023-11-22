f6d03a153aa0b5344d242623ce3b8b2d48c55987
Tachy-22 <entekumejeffrey@gmail.com>
2023-11-23 00:09:29 +0100

    commit

--- frontend/src/pages/cart/Cart.jsx
+++ frontend/src/pages/cart/Cart.jsx
@@ -17,7 +17,7 @@ const Cart = () => {
   }, [getMyCart]);
 
   return (
-    <div className=" flex flex-col md:gap-[3.5rem] gap-4 xl:max-w-[90rem] mx-auto justify-center items-center xl:px-[10rem] md:p-[4rem] p-2   w-full -2 border-gray-400 h-max flex-grow lg:pt-[4rem]  pt-[3rem]  bg-gray-100 bg-gradient-to-r from-[10%] via-gray-100 from-black/20  carousel duration-[1s] bg-center transition-all xl:border- ">
+    <div className=" flex flex-col md:gap-[3.5rem] gap-4 xl:max-w-[90rem] mx-auto justify-start items-center xl:px-[10rem] md:p-[4rem] p-2   w-full -2 border-gray-400 h-max flex-grow lg:pt-[4rem]  pt-[3rem]  bg-gray-100 bg-gradient-to-r from-[10%] via-gray-100 from-black/20  carousel duration-[1s] bg-center transition-all xl:border- ">
       {myCart && myCart.length > 0 && (
         <div className="w-full h-full flex flex-col gap-2 ">
           <h2 className=" text-2xl h-max font-bold w-full text-start flex gap-2 border-green-300 pb-4 items-center">
