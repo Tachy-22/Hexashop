e9aa6de400d78cb566efdb0813fe898c50e524d1
Tachy-22 <entekumejeffrey@gmail.com>
2023-11-23 00:22:33 +0100

    commit

--- frontend/src/pages/home/components/HeroCardA.jsx
+++ frontend/src/pages/home/components/HeroCardA.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/prop-types */
 
+import { useMemo } from "react";
+
 //import { Link } from "react-router-dom";
 
 const HeroCardA = ({ image }) => {
+  const imgUrl = useMemo(() => image, [image]);
   return (
     <div
-      style={{ "--image-url": `url(${image})` }}
+      style={{ "--image-url": `url(${imgUrl})` }}
       className="bg-[image:var(--image-url)]  rounded bg-cover h-full  flex justify-center items-center text-white bg-no-repeat bg-center duration-[3s] hover:bg-left-bottom   transition-all cursor-pointer "
     >
       <div className="flex flex-col md:gap-4 gap-2 justify-center items-center  h-full w-full backdrop-brightness-50">
@@ -21,12 +24,6 @@ const HeroCardA = ({ image }) => {
         <p className=" text-center text-white/80  sm:text-[1rem] text-sm">
           Welcome to Hexashop - Your Ultimate Fashion Destination
         </p>
-        {/* <Link
-          to="/"
-          className="border border-gray-500 rounded-md p-2 text-xl px-2 animate-blink-blue-pink hover:border-blue-500 hover:bg-blue-500 hover:text-white bg-black/75 text-white hover:scale-110 transition-all duration-700"
-        >
-          Sign In
-        </Link> */}
       </div>
     </div>
   );
